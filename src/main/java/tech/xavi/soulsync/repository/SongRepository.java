2370cbb650ffdd146b3d58efa6108179178156ff
Xavi <mail@xavi.tech>
2023-10-22 02:38:38 +0200

    Refactor FileFinderService and minor fixes

--- src/main/java/tech/xavi/soulsync/repository/SongRepository.java
+++ src/main/java/tech/xavi/soulsync/repository/SongRepository.java
@@ -4,9 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tech.xavi.soulsync.entity.Song;
 
-import java.util.UUID;
-
 @Repository
 public interface SongRepository extends JpaRepository<Song, Integer> {
-    Song findBySearchId(UUID id);
+    Song findByFilename(String filename);
 }
